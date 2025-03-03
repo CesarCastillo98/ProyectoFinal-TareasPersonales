79d450fd8eaba58add0600f8ba95d1b2d881919a
CesarCastillo98 <castillocesarmf@gmail.com>
2025-03-03 16:14:55 -0500

    añadi los controller

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,9 @@
+export const getAllTasks = (req,res)=> res.send("Obteniendo Tareas")
+
+export const getTask = (req,res)=> res.send("Obteniendo Tarea Unica")
+
+export const createTask = (req,res)=> res.send("Creando Tareas")
+
+export const updateTask = (req,res)=> res.send("Actualizando Tarea Unica")
+
+export const deleteTask = (req,res)=> res.send("Eliminando Tarea")
